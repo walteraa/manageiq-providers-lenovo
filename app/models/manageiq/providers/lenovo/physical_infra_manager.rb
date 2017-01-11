0924e164248a9062e28c2585f6a3f09232f54dce
skovic <skovic@lenovo.com>
2017-01-11 12:36:36 -0500

    Add support for turning on/off LEDs - working ver

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -16,4 +16,28 @@ class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::I
   def self.description
     @description ||= "Lenovo XClarity"
   end
+
+  def turn_on_loc_led(server, options = {})
+    # Connect to the LXCA instance
+    auth = authentications.first
+    endpoint = endpoints.first
+    client = connect({:user => auth.userid,
+                      :pass => auth.password,
+                      :host => endpoint.hostname})
+
+    # Turn on the location LED using the xclarity_client API
+    client.turn_on_loc_led(server.uuid)
+  end
+
+  def turn_off_loc_led(server, options = {})
+    # Connect to the LXCA instance
+    auth = authentications.first
+    endpoint = endpoints.first
+    client = connect({:user => auth.userid,
+                      :pass => auth.password,
+                      :host => endpoint.hostname})
+
+    # Turn off the location LED using the xclarity_client API
+    client.turn_off_loc_led(server.uuid)
+  end
 end
